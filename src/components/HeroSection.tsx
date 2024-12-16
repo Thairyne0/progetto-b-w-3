663b469138b1e67027f5e6dc0c8b8a63169b76a9
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-16 12:16:07 +0100

    useState per :hover

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,10 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
 
 const HeroSection = () => {
+    const [hoveredButton1, setHoveredButton1] = useState(false);
+    const [hoveredButton2, setHoveredButton2] = useState(false);
+
   const imgHeroStyle = {
     borderRadius: "50%",
     width: "9em",
@@ -14,7 +17,7 @@ const HeroSection = () => {
   };
 
   const cameraButtonStyle = {
-    position: "absolute" as React.CSSProperties['position'],
+    position: "absolute" as React.CSSProperties["position"],
     top: "15%",
     right: "30%",
     zIndex: 2,
@@ -22,16 +25,24 @@ const HeroSection = () => {
     border: "none",
     borderRadius: "50%",
     padding: "0.5em",
-   
   };
 
+  const buttonStyles = (hovered: boolean) => ({
+    backgroundColor: hovered ? 'blue' : 'white',
+    border: 'blue 1px solid',
+    color: hovered ? 'white' : 'blue',
+    margin: '0.5em',
+    borderRadius: '30px',
+    padding: '0.25em 1em',
+    cursor: 'pointer',
+  });
 
   const containerImg = {
-    position: "relative" as React.CSSProperties['position'],
-     display: "inline-block" ,
-     left: '10%',
-     top: '90%'
-  }
+    position: "relative" as React.CSSProperties["position"],
+    display: "inline-block",
+    left: "10%",
+    top: "90%",
+  };
   return (
     <>
       <section className="m-5">
@@ -46,9 +57,11 @@ const HeroSection = () => {
             // zIndex: 1,
           }}
         >
-            <div style={containerImg}>
-          <img style={imgHeroStyle} src="https://placecats.com/300/200"></img>
-          <button style={cameraButtonStyle}><i className="bi bi-camera" ></i></button>
+          <div style={containerImg}>
+            <img style={imgHeroStyle} src="https://placecats.com/300/200"></img>
+            <button style={cameraButtonStyle}>
+              <i className="bi bi-camera"></i>
+            </button>
           </div>
         </div>
         <div className="border border-2 p-5 ">
@@ -63,8 +76,18 @@ const HeroSection = () => {
             Informazioni di contatto
           </a>
           <div className="hero-buttons">
-            <button>Modifica Profilo</button>
-            <button>Aggiungi sezione del profilo</button>
+            <button
+              style={buttonStyles(hoveredButton1)}
+              onMouseEnter={() => setHoveredButton1(true)}
+              onMouseLeave={() => setHoveredButton1(false)}
+            >
+              Modifica Profilo
+            </button>
+            <button
+              style={buttonStyles(hoveredButton2)}
+              onMouseEnter={() => setHoveredButton2(true)}
+              onMouseLeave={() => setHoveredButton2(false)}>
+                Aggiungi sezione del profilo</button>
           </div>
         </div>
       </section>
