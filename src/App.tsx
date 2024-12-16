a287b74559086602d66e5eeb3049f69a8f6a25a1
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-16 15:43:44 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import './App.css'
-import HeroSection from './components/HeroSection'
-import EditProfile from './components/EditProfile'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
  
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HeroSection />} />       
-        <Route path="/edit-profile" element={<EditProfile />} /> 
-      </Routes>
-    </Router>
-  )
+
 }
 
 export default App
