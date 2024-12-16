5f90fbf5e1027e1a38d2b885cf63594c49855c56
Patricia <patricia.badji89@gmail.com>
2024-12-16 19:03:27 +0100

    Create AddExperience.tsx

---
+++ src/components/AddExperience.tsx
@@ -0,0 +1,142 @@
+import { ChangeEvent, useState } from "react"
+import { Button, Form } from "react-bootstrap"
+
+interface ExperienceForm {
+    role: string,
+    company: string,
+    startDate: string,
+    endDate: string | null, // può essere null
+    description: string,
+    area: string,
+    hybrid: boolean
+
+}
+
+const AddExperience = () => {
+
+    const [form, setForm] = useState<ExperienceForm>({
+
+        role: '',
+        company: '',
+        startDate: '',
+        endDate: null, // può essere null
+        description: '',
+        area: '',
+        hybrid: false
+
+    })
+
+    const [image, setImage] = useState<File | null>(null),  //oggetto file
+
+
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+    //deve fare una post 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append("role", form.role);
+        formData.append("company", form.company);
+        formData.append("startDate", form.startDate);
+        formData.append("endDate", form.endDate || "");
+        formData.append("description", form.description);
+        formData.append("area", form.area);
+        formData.append("hybrid", form.hybrid.toString())
+        //se c'è l'immagine, la metto
+        if (image) {
+            formData.append("image", image);
+        }
+
+
+        try {
+            const response = await fetch('https://striveschool-api.herokuapp.com/api/profile/:userId/experiences', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzYwNDA0YTBlYTI4NjAwMTUyOGI5NjkiLCJpYXQiOjE3MzQzNjExNjIsImV4cCI6MTczNTU3MDc2Mn0.8TjyYQRYtNsiPmjF9yNjLl5vAhFkkg2SpoSrjwrFVE0'
+                },
+                body: formData,
+            })
+
+
+            if (response.ok) {
+
+                setForm({
+                    role: '',
+                    company: '',
+                    startDate: '',
+                    endDate: null,
+                    description: '',
+                    area: '',
+                    hybrid: false
+                })
+
+
+            } else { throw new Error("Errore nel caricamento dell'esperienza") }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        // name è role
+        const { name, value, type, checked } = e.target;  //mi prende quello che digito
+        setForm((form) => ({
+            ...form,
+
+
+            [name]: type === 'checkbox' ? checked : value
+        }))
+
+    }
+
+    return (
+
+        <Form onSubmit={handleSubmit} >
+            <Form.Group className="mb-3" >
+                <Form.Label>Ruolo</Form.Label>
+                <Form.Control type="text" placeholder="Ruolo ricoperto" name="role" value={form.role} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Azienda</Form.Label>
+                <Form.Control type="text" placeholder="Nome azienda" name="company" value={form.company} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Data inizio</Form.Label>
+                <Form.Control type="month" name="startDate" value={form.startDate} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Data fine</Form.Label>
+                {/* può non avere una fine, metto O stringa vuota */}
+                <Form.Control type="month" name="endDate" value={form.endDate || ''} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Descrizione</Form.Label>
+                <Form.Control type="text" placeholder="I tuoi compiti all'interno dell'azienda" name="description" value={form.description} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Sede dell'azienda</Form.Label>
+                <Form.Control type="text" placeholder="Luogo in cui ha sede l'azienda" name="area" value={form.area} onChange={handleInput} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Ibrido/Smart Working" name="hybrid" checked={form.hybrid} onChange={handleInput} /></Form.Group>
+            <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Aggiungi un'immagine</Form.Label>
+                <input type="file" accept="image/*" onChange={handleImageChange} />
+                {/* così accetta solo file che sono immagini */}
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default AddExperience
