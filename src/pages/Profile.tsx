54327c48915292ae546f93058e2dfc2e75a041a4
Vale-n-tina <valentia.grg@gmail.com>
2024-12-17 11:36:38 +0100

    .

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import MyExperience from "../components/MyExperience";
 import MyFooter from "../components/MyFooter";
@@ -7,6 +8,19 @@ import TokenProps from "../types/Hero";
 
 
 const Profile = (props:TokenProps) => {
+  const [profileData, setProfileData] = useState({
+    name: "",
+    surname: "",
+    username: "",
+    bio: "",
+    title: "",
+    area: "",
+    image: "",
+    _id:"",
+  });
+  const updateProfileData = (newData: Partial<typeof profileData>) => {
+    setProfileData((prevData) => ({ ...prevData, ...newData }));
+  };
 
   return (
     <div>
@@ -16,8 +30,9 @@ const Profile = (props:TokenProps) => {
       <main>
         <div className="row justify-content-center align-items-top g-0">
           <div className="col-8 col-md-7">
-            <HeroSection token={props.token}/>
-            <MyExperience token={props.token}/>
+            <HeroSection token={props.token} profileData={profileData} 
+              updateProfileData={updateProfileData} />
+            <MyExperience profilo={profileData}  token={props.token} />
           </div>
           <div className="col-8 col-md-3 mt-5">
             <ContactsList></ContactsList>
