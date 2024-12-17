8428b730f4d4d086ee0162f4249b94ad9782e921
Vale-n-tina <valentia.grg@gmail.com>
2024-12-17 10:14:21 +0100

    .

--- src/components/MyExperience.tsx
+++ src/components/MyExperience.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import CardExperience from "./CardExperience";
 import IExperience from "../types/Experience";
 import TokenProps from "../types/Hero";
+import { Card, Row, Col, Button, } from "react-bootstrap";
 
 
 const MyExperience = (props:TokenProps) => {
@@ -37,6 +38,29 @@ const MyExperience = (props:TokenProps) => {
     <>
     <section className="m-0 mt-5 m-md-5 bg-white rounded-3">
   <h4>Experience</h4>
+  <Col className="col col-10 border border-muted  rounded-2 ms-3 ps-2" >
+        <h5  className="text-muted m-0">Esperienza</h5>
+         <small className="m-0 text-secondary mb-2">Metti in risalto i risultati raggiunti e ottieni fino a 2 volte in più visualizzazioni del profilo e collegamenti  </small>
+      <Row className="g-2">
+        <Col className="col-2 col-md-2 mt-3 col-lg-1 ">
+        <i className="bi bi-briefcase-fill px-3 py-2 border border-secondary text-muted rounded-2"></i>
+        </Col>
+        <Col className="col-10 col-md-10 col-lg-11">
+          <Card.Body>
+            <small className="m-0 text-muted">Qualifica</small>
+            <Card.Text className="text-muted m-0">
+              <small>organizzazione</small>
+              <br />
+              
+              <small className="text-muted">2023-presente</small>
+            </Card.Text>
+            <Button variant="outline-primary" size="sm">Aggiungi esperienza</Button>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Col>
+            
+          
     {experiences.map((e) => {
     return (
     <CardExperience experience={e} />)
