00e7cbe78a287864723472205eb438d2a3d9a742
antgian96 <giannattasioa55@gmail.com>
2024-12-17 15:26:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+<<<<<<< Updated upstream
 
 import { useState } from 'react'
 import './App.css'
 import PageAccedi from './components/PageAccedi'
+=======
+import { useState } from "react";
+import "./App.css";
+import EditProfile from "./components/EditProfile";
+import PageAccedi from "./components/PageAccedi";
+import NewsFeed from "./components/NewsFeed";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddExperience from "./components/AddExperience";
+>>>>>>> Stashed changes
 
 function App() {
   // stato
@@ -21,7 +32,11 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
       <PageAccedi token={token} ChangeToken={HandletokenChange} onSubmit={handleSubmit} />
+=======
+     
+>>>>>>> Stashed changes
     </>
   )
 }
